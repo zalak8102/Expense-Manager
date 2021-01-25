73bb536c4463b401c59cd8d46d49de830c8cf11e
zalak8102 <zalakbhingradiya@gmail.com>
2021-01-25 17:59:27 +0530

    expense,report template

--- SDP_Project/expense/views.py
+++ SDP_Project/expense/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from expense.models import expense
+from django.contrib import messages
 
 # Create your views here.
+def expense(request):
+    return render(request,'expense/expense.html')
+
+def report(request):
+    return render(request,'expense/report.html')
